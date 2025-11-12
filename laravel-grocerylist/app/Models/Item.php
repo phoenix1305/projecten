3c085daaab0474759416c51e7e18e580099332f5
phoenix1305 <t.bramsleven@xs4all.nl>
2025-11-12 13:52:44 +0100

    first commit laravel projecten

---
+++ laravel-grocerylist/app/Models/Item.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    public function category() {
+    return $this->belongsTo(Category::class);
+}
+}
