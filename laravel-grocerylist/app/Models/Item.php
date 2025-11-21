3412694dc0a773bab4b856d5f93e8d7d4d0d18a1
phoenix1305 <t.bramsleven@xs4all.nl>
2025-11-21 13:20:46 +0100

    second commit

--- laravel-grocerylist/app/Models/Item.php
+++ laravel-grocerylist/app/Models/Item.php
@@ -3,10 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
-    public function category() {
-    return $this->belongsTo(Category::class);
-}
+    use HasFactory;
+
+     protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
